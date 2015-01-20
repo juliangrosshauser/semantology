b5c8397440387a55bcae2d196f64af86616a1d6f
Julian Grosshauser <julian@juliangrosshauser.com>
2015-01-20 23:28:53 +0100

    Add module "Semantology"

--- lib/semantology.rb
+++ lib/semantology.rb
@@ -1,1 +1,31 @@
+require 'rdf'
+require 'linkeddata'
 
+# The module that contains everything Semantology-related
+module Semantology
+  # Convinence method
+  # @param input_path [String] path to input file. can be local or a website
+  # @param input_format [Symbol] format of input data.
+  # can be `:rdfa`, `microdata` or `microformat`
+  # @param output_path [String] path to output file
+  # @param output_format [Symbol] format of output data. can only be `turtle`
+  def self.translate(input_path, input_format, output_path, output_format)
+    case input_format
+    when :rdfa
+      fail NotImplementedError, 'RDFa is not implemented'
+    when :microdata
+      fail NotImplementedError, 'Microdata is not implemented'
+    when :microformat
+      fail NotImplementedError, 'Microformat is not implemented'
+    else
+      fail ArgumentError, 'Specified input format is not supported'
+    end
+
+    case output_format
+    when :turtle
+      fail NotImplementedError, 'Turtle is not implemented'
+    else
+      fail ArgumentError, 'Specified output format is not supported'
+    end
+  end
+end
